a872976376080470eb6c1e9a4f26d927763f0538
Jim Hayes <jhayes@sdsc.edu>
2014-10-28 08:36:15 -0700

    Adapt Rocks method for producing modulefiles.

--- src/visit-modules/version.mk
+++ src/visit-modules/version.mk
@@ -1,4 +1,12 @@
-NAME    	= visit-modules
-VERSION 	= 1.0
-RELEASE         = 6
-RPM.EXTRAS      = AutoReq:No
+PACKAGE     = visit
+CATEGORY    = applications
+
+NAME        = $(PACKAGE)-modules
+RELEASE     = 1
+PKGROOT     = /opt/modulefiles/$(CATEGORY)/$(PACKAGE)
+
+VERSION_SRC = $(REDHAT.ROOT)/src/$(PACKAGE)/version.mk
+VERSION_INC = version.inc
+include $(VERSION_INC)
+
+RPM.EXTRAS  = AutoReq:No
